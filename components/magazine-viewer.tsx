354b42fa48eb6d3cc8dd1bd68661efa64a7d8f09
Niyuhhh <eliott.coffre@gmail.com>
2025-08-26 16:52:29 +0200

    feat: move pagination to bottom left

--- components/magazine-viewer.tsx
+++ components/magazine-viewer.tsx
@@ -328,7 +328,7 @@ export function MagazineViewer({ pages }: MagazineViewerProps) {
         <ChevronRight className="h-8 w-8" />
       </Button>
 
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2">
+      <div className="absolute bottom-4 left-4">
         <Pagination
           totalPages={totalPages}
           currentPage={currentPage + 1}
