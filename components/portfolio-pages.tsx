897a11e5a92a3170155ded6e83318a28bc5c799c
Niyuhhh <eliott.coffre@gmail.com>
2025-08-27 14:04:40 +0200

    feat: style visit button

--- components/portfolio-pages.tsx
+++ components/portfolio-pages.tsx
@@ -72,7 +72,7 @@ const bjornChapterPages = [
         >
           <Button
             variant="outline"
-            className="absolute bottom-30 left-[51%] z-10 -translate-x-1/2 rounded-none border border-gray-400 bg-white px-8 py-3 font-sora text-xs text-gray-700 hover:bg-gray-100"
+            className="absolute bottom-30 left-[51%] z-10 -translate-x-1/2 rounded-none border border-gray-400 bg-white px-8 py-3 font-sora text-xs text-gray-700 hover:bg-[#1C1C1C] hover:text-white"
           >
             visiter le site
           </Button>
