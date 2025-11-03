216513f1268d0e73ff372de315f845210aca523f
Niyuhhh <eliott.coffre@gmail.com>
2025-11-03 11:23:20 +0100

    Add animated overlay to portfolio page 35

--- components/portfolio-pages.tsx
+++ components/portfolio-pages.tsx
@@ -489,6 +489,14 @@ export const portfolioPages = [
     content: (
       <div className="relative w-full h-full">
         <CloudinaryImage src={page35} alt="Portfolio Page 35" />
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDN6ZXp1OXZtaW5sMXg1eTRzMXdkc242NHVobmh6Z2ljNmtkdjJkYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L6J78MhsbfUmPdRl3N/giphy.gif"
+          alt="Animation illustrant le projet"
+          width={2050}
+          height={1170}
+          className="absolute"
+          style={{ left: "268px", bottom: "840px" }}
+        />
       </div>
     ),
   },
